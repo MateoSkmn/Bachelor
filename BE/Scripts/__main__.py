73f04f396365eeb0e6d7575797add0896e097147
MateoSkmn <sakomanmateo2812@gmail.com>
2024-03-27 10:43:18 +0100

    Endpunkte hinzugefügt
    - Alle benötigten Endpunkte eingefügt

--- BE/Scripts/__main__.py
+++ BE/Scripts/__main__.py
@@ -9,8 +9,9 @@ CORS(app, origins=['http://localhost:4200', 'http://localhost:4200/*'])
 ######
 
 ### ROUTES ###
+# GET
 @app.route('/', methods=['GET'])
-def hello_world():
+def base_route():
     return jsonify('Mateo Sakoman - 1865383')
 
 @app.route('/data/record', methods=['GET'])
@@ -20,6 +21,36 @@ def get_records():
 @app.route('/data/model', methods=['GET'])
 def get_models():
     return jsonify("Alle Modelle")
+
+@app.route('/data/records/<file_name>/<id>/lime', methods=['GET'])
+def get_lime(file_name, id):
+    return jsonify("Get LIME values for: " + file_name + "/" + id)
+
+# POST
+@app.route('/data/record', methods=['POST'])
+def upload_record():
+    return jsonify("Upload Record")
+
+@app.route('/data/model', methods=['POST'])
+def upload_model():
+    return jsonify("Upload Model")
+
+@app.route('/data/user-info/connection', methods=['POST'])
+def upload_user_info_connection():
+    return jsonify("Upload Connection")
+
+@app.route('/data/user-info/evaluation/<file_name>', methods=['POST'])
+def upload_user_info_evaluation(file_name):
+    return jsonify("Upload Evaluation for " + file_name)
+
+# DELETE
+@app.route('/data/record/<file_name>', methods=['DELETE'])
+def delete_record(file_name):
+    return jsonify("Delete Record: " +  file_name)
+
+@app.route('/data/model/<file_name>', methods=['DELETE'])
+def delete_model(file_name):
+    return jsonify("Delete Model: " +  file_name)
 ######
 
 if __name__ == '__main__':
